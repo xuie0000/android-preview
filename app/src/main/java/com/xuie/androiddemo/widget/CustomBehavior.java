102cbd53180a6562e2b265c83b5a782405d7d272
Xu Jie <xujie@yf-space.com>
2016-09-06 15:13:15 +0800

    custom behavior

--- app/src/main/java/com/xuie/androiddemo/widget/CustomBehavior.java
+++ app/src/main/java/com/xuie/androiddemo/widget/CustomBehavior.java
@@ -7,6 +7,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.v4.view.ViewCompat;
 import android.support.v4.view.ViewPropertyAnimatorListener;
 import android.support.v4.view.animation.FastOutSlowInInterpolator;
+import android.support.v4.widget.NestedScrollView;
 import android.util.AttributeSet;
 import android.view.View;
 import android.view.animation.Animation;
@@ -17,30 +18,50 @@ import android.view.animation.Interpolator;
  * Created by xuie on 16-9-6.
  * From Drakeet <drakeet.me@gmail.com> Meizhi
  */
-public class CustomBehavior extends CoordinatorLayout.Behavior<FloatingActionButton>{
+public class CustomBehavior extends CoordinatorLayout.Behavior<FloatingActionButton> {
     private static final String TAG = "CustomBefavior";
     private static final Interpolator INTERPOLATOR = new FastOutSlowInInterpolator();
     private boolean mIsAnimatingOut = false;
+    private boolean mIsScrollUp = false;
+
+    private float previousY = 0f;
 
     public CustomBehavior(Context context, AttributeSet attrs) {
         super(context, attrs);
     }
 
-    @Override public boolean onStartNestedScroll(CoordinatorLayout coordinatorLayout, FloatingActionButton child, View directTargetChild, View target, int nestedScrollAxes) {
-        return nestedScrollAxes == ViewCompat.SCROLL_AXIS_VERTICAL || super.onStartNestedScroll(coordinatorLayout, child, directTargetChild, target, nestedScrollAxes);
+//    @Override public boolean onStartNestedScroll(CoordinatorLayout coordinatorLayout, FloatingActionButton child, View directTargetChild, View target, int nestedScrollAxes) {
+//        return nestedScrollAxes == ViewCompat.SCROLL_AXIS_VERTICAL || super.onStartNestedScroll(coordinatorLayout, child, directTargetChild, target, nestedScrollAxes);
+//    }
+//
+//    @Override public void onNestedScroll(CoordinatorLayout coordinatorLayout, FloatingActionButton child, View target, int dxConsumed, int dyConsumed, int dxUnconsumed, int dyUnconsumed) {
+////        Log.d(TAG, "dxConsumed:" + dxConsumed + ", dyConsumed:" + dyConsumed + ", dxUnconsumed:" + dxUnconsumed + ", dyUnconsumed:" + dyUnconsumed);
+//        if (dyUnconsumed > 0 && !this.mIsAnimatingOut && child.getVisibility() == View.VISIBLE) {
+//            // User scrolled down and the FAB is currently visible -> hide the FAB
+//            animateOut(child);
+//        } else if (dyUnconsumed < 0 && child.getVisibility() != View.VISIBLE) {
+//            // User scrolled up and the FAB is currently not visible -> show the FAB
+//            animateIn(child);
+//        }
+//
+//        super.onNestedScroll(coordinatorLayout, child, target, dxConsumed, dyConsumed, dxUnconsumed, dyUnconsumed);
+//    }
+
+    @Override public boolean layoutDependsOn(CoordinatorLayout parent, FloatingActionButton child, View dependency) {
+        return dependency instanceof NestedScrollView;
     }
 
-    @Override public void onNestedScroll(CoordinatorLayout coordinatorLayout, FloatingActionButton child, View target, int dxConsumed, int dyConsumed, int dxUnconsumed, int dyUnconsumed) {
-//        Log.d(TAG, "dxConsumed:" + dxConsumed + ", dyConsumed:" + dyConsumed + ", dxUnconsumed:" + dxUnconsumed + ", dyUnconsumed:" + dyUnconsumed);
-        if (dyUnconsumed > 0 && !this.mIsAnimatingOut && child.getVisibility() == View.VISIBLE) {
-            // User scrolled down and the FAB is currently visible -> hide the FAB
-            animateOut(child);
-        } else if (dyUnconsumed < 0 && child.getVisibility() != View.VISIBLE) {
-            // User scrolled up and the FAB is currently not visible -> show the FAB
+    @Override public boolean onDependentViewChanged(CoordinatorLayout parent, FloatingActionButton child, View dependency) {
+        float y = dependency.getY();
+//        Log.d(TAG, "onDependentViewChanged: " + y + ", " + previousY + ", " + (child.getVisibility() == View.VISIBLE));
+        if (y - previousY > 0 && child.getVisibility() != View.VISIBLE) {
             animateIn(child);
+        } else if (y - previousY < 0 && child.getVisibility() == View.VISIBLE) {
+            animateOut(child);
         }
 
-        super.onNestedScroll(coordinatorLayout, child, target, dxConsumed, dyConsumed, dxUnconsumed, dyUnconsumed);
+        previousY = dependency.getY();
+        return true;
     }
 
     // Same animation that FloatingActionButton.Behavior uses to hide the FAB when the AppBarLayout exits
