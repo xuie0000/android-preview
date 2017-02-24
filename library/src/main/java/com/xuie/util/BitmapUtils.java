a3fcdd875c4ca4983f84d19ae55b31ca7da0dc0b
Xu Jie <xujie@yf-space.com>
2017-02-24 16:16:04 +0800

    add utils

--- library/src/main/java/com/xuie/util/BitmapUtils.java
+++ library/src/main/java/com/xuie/util/BitmapUtils.java
@@ -1,242 +1,214 @@
 package com.xuie.util;
 
-import android.content.ContentResolver;
+import android.annotation.TargetApi;
+import android.content.ContentUris;
 import android.content.Context;
-import android.content.res.Resources;
+import android.database.Cursor;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.Canvas;
-import android.graphics.PixelFormat;
-import android.graphics.drawable.BitmapDrawable;
-import android.graphics.drawable.Drawable;
 import android.net.Uri;
-import android.support.annotation.AnyRes;
-import android.support.v4.content.ContextCompat;
+import android.os.Build;
+import android.os.Environment;
+import android.provider.DocumentsContract;
+import android.provider.MediaStore;
 
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
+import com.xuie.util.config.FileConfig;
+
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.nio.channels.FileChannel;
+import java.util.Calendar;
 
-/**
- * Created by xuie on 15-11-28.
- */
 public class BitmapUtils {
 
-    public static File Drawable2File(Context context, int drawId, String path) {
-        try {
-            File f = new File(path);
-            InputStream inputStream = context.getResources().openRawResource(drawId);
-            OutputStream out = new FileOutputStream(f);
-            byte buf[] = new byte[1024];
-            int len;
+    public static File saveUriToFile(Context context, Uri croppedFileUri) throws Exception {
+        String path = FileConfig.SELETE_IMG_PATH;
 
-            while ((len = inputStream.read(buf)) > 0) {
-                out.write(buf, 0, len);
+//        String name = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String filename = String.format("%d_%s", Calendar.getInstance().getTimeInMillis(), croppedFileUri.getLastPathSegment()) + ".jpg";
+        if (!new File(path).exists()) {
+            new File(path).mkdirs();
+        }
+        File saveFile = new File(path, filename);
+        FileInputStream inStream = new FileInputStream(new File(getPath(context, croppedFileUri)));
+        FileOutputStream outStream = new FileOutputStream(saveFile);
+        FileChannel inChannel = inStream.getChannel();
+        FileChannel outChannel = outStream.getChannel();
+        inChannel.transferTo(0, inChannel.size(), outChannel);
+        inStream.close();
+        outStream.close();
+
+        return saveFile;
+    }
+
+
+    @TargetApi(Build.VERSION_CODES.KITKAT)
+    public static String getPath(final Context context, final Uri uri) {
+
+        final boolean isKitKat = Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT;
+
+        // DocumentProvider
+        if (isKitKat && DocumentsContract.isDocumentUri(context, uri)) {
+            // ExternalStorageProvider
+            if (isExternalStorageDocument(uri)) {
+                final String docId = DocumentsContract.getDocumentId(uri);
+                final String[] split = docId.split(":");
+                final String type = split[0];
+
+                if ("primary".equalsIgnoreCase(type)) {
+                    return Environment.getExternalStorageDirectory() + "/" + split[1];
+                }
+
+                // TODO handle non-primary volumes
             }
-            out.close();
-            inputStream.close();
-            return f;
+            // DownloadsProvider
+            else if (isDownloadsDocument(uri)) {
+
+                final String id = DocumentsContract.getDocumentId(uri);
+                final Uri contentUri = ContentUris.withAppendedId(
+                        Uri.parse("content://downloads/public_downloads"), Long.valueOf(id));
+
+                return getDataColumn(context, contentUri, null, null);
+            }
+            // MediaProvider
+            else if (isMediaDocument(uri)) {
+                final String docId = DocumentsContract.getDocumentId(uri);
+                final String[] split = docId.split(":");
+                final String type = split[0];
+
+                Uri contentUri = null;
+                if ("image".equals(type)) {
+                    contentUri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
+                } else if ("video".equals(type)) {
+                    contentUri = MediaStore.Video.Media.EXTERNAL_CONTENT_URI;
+                } else if ("audio".equals(type)) {
+                    contentUri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
+                }
+
+                final String selection = "_id=?";
+                final String[] selectionArgs = new String[]{
+                        split[1]
+                };
+
+                return getDataColumn(context, contentUri, selection, selectionArgs);
+            }
+        }
+        // MediaStore (and general)
+        else if ("content".equalsIgnoreCase(uri.getScheme())) {
+
+            // Return the remote address
+            if (isGooglePhotosUri(uri))
+                return uri.getLastPathSegment();
+
+            return getDataColumn(context, uri, null, null);
+        }
+        // File
+        else if ("file".equalsIgnoreCase(uri.getScheme())) {
+            return uri.getPath();
+        }
+
+        return null;
+    }
+
+    /**
+     * Get the value of the data column for this Uri. This is useful for
+     * MediaStore Uris, and other file-based ContentProviders.
+     *
+     * @param context       The context.
+     * @param uri           The Uri to query.
+     * @param selection     (Optional) Filter used in the query.
+     * @param selectionArgs (Optional) Selection arguments used in the query.
+     * @return The value of the _data column, which is typically a file path.
+     */
+    public static String getDataColumn(Context context, Uri uri, String selection,
+                                       String[] selectionArgs) {
+
+        Cursor cursor = null;
+        final String column = "_data";
+        final String[] projection = {
+                column
+        };
+
+        try {
+            cursor = context.getContentResolver().query(uri, projection, selection, selectionArgs,
+                    null);
+            if (cursor != null && cursor.moveToFirst()) {
+                final int index = cursor.getColumnIndexOrThrow(column);
+                return cursor.getString(index);
+            }
+        } finally {
+            if (cursor != null)
+                cursor.close();
+        }
+        return null;
+    }
+
+
+    /**
+     * @param uri The Uri to check.
+     * @return Whether the Uri authority is ExternalStorageProvider.
+     */
+    public static boolean isExternalStorageDocument(Uri uri) {
+        return "com.android.externalstorage.documents".equals(uri.getAuthority());
+    }
+
+    /**
+     * @param uri The Uri to check.
+     * @return Whether the Uri authority is DownloadsProvider.
+     */
+    public static boolean isDownloadsDocument(Uri uri) {
+        return "com.android.providers.downloads.documents".equals(uri.getAuthority());
+    }
+
+    /**
+     * @param uri The Uri to check.
+     * @return Whether the Uri authority is MediaProvider.
+     */
+    public static boolean isMediaDocument(Uri uri) {
+        return "com.android.providers.media.documents".equals(uri.getAuthority());
+    }
+
+    /**
+     * @param uri The Uri to check.
+     * @return Whether the Uri authority is Google Photos.
+     */
+    public static boolean isGooglePhotosUri(Uri uri) {
+        return "com.google.android.apps.photos.content".equals(uri.getAuthority());
+    }
+
+
+    /**
+     * 保存bitmap图片
+     *
+     * @param bm
+     * @return
+     */
+
+    long time;//用来作为图片的名字
+
+    public Uri saveBitmap(Bitmap bm) {
+        File tmpDir = new File(Environment.getExternalStorageDirectory() + "/com.***");//新建文件夹用来保存图片，地址自己写
+        if (!tmpDir.exists()) {
+            tmpDir.mkdir();
+        }//如果地址不存在，则新建
+        if (time == 0) {
+            time = System.currentTimeMillis();
+        }//获取系统时间
+        File img = new File(tmpDir.getAbsolutePath() + "/" + time + ".png");
+        try {
+            FileOutputStream fos = new FileOutputStream(img);
+            bm.compress(Bitmap.CompressFormat.PNG, 85, fos);//压缩文件
+            fos.flush();
+            fos.close();
+            return Uri.fromFile(img);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            return null;
         } catch (IOException e) {
             e.printStackTrace();
-        }
-        return null;
-    }
-
-    public static Uri File2Uri(String path) {
-        return File2Uri(new File(path));
-    }
-
-    public static Uri File2Uri(File file) {
-        return Uri.fromFile(file);
-    }
-
-    // http://stackoverflow.com/questions/6602417/get-the-uri-of-an-image-stored-in-drawable
-    // Uri uri = Uri.parse("android.resource://your.package.here/drawable/image_name");
-    // InputStream stream = getContentResolver().openInputStream(uri);
-    // 使用分享时不能用这个方法
-    // You want to share an image from drawable resource. But other apps have no access to your resources. There are two ways to solve this. 1. Use a content provider. 2. Copy the file to external/public memory first.
-    public static Uri Drawable2Uri(Context context, @AnyRes int drawId) {
-        return Uri.parse(ContentResolver.SCHEME_ANDROID_RESOURCE +
-                "://" + context.getResources().getResourcePackageName(drawId)
-                + '/' + context.getResources().getResourceTypeName(drawId)
-                + '/' + context.getResources().getResourceEntryName(drawId));
-    }
-
-    public static Drawable Uri2Drawable(Context context, Uri uri) {
-        try {
-            InputStream inputStream = context.getContentResolver().openInputStream(uri);
-            return Drawable.createFromStream(inputStream, uri.toString());
-        } catch (FileNotFoundException e) {
-//            return context.getResources().getDrawable(R.mipmap.ic_launcher);
             return null;
         }
-    }
-
-    // 将byte[]转换成InputStream
-    public static InputStream Byte2InputStream(byte[] b) {
-        ByteArrayInputStream bais = new ByteArrayInputStream(b);
-        return bais;
-    }
-
-    // 将InputStream转换成byte[]
-    public static byte[] InputStream2Bytes(InputStream is) {
-        String str = "";
-        byte[] readByte = new byte[1024];
-        int readCount = -1;
-        try {
-            while ((readCount = is.read(readByte, 0, 1024)) != -1) {
-                str += new String(readByte).trim();
-            }
-            return str.getBytes();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
-
-    // 将Bitmap转换成InputStream
-    public static InputStream Bitmap2InputStream(Bitmap bm) {
-        ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        bm.compress(Bitmap.CompressFormat.JPEG, 100, baos);
-        InputStream is = new ByteArrayInputStream(baos.toByteArray());
-        return is;
-    }
-
-    // 将Bitmap转换成InputStream
-    public static InputStream Bitmap2InputStream(Bitmap bm, int quality) {
-        ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        bm.compress(Bitmap.CompressFormat.PNG, quality, baos);
-        InputStream is = new ByteArrayInputStream(baos.toByteArray());
-        return is;
-    }
-
-    // 将InputStream转换成Bitmap
-    public static Bitmap InputStream2Bitmap(InputStream is) {
-        return BitmapFactory.decodeStream(is);
-    }
-
-    // Drawable转换成InputStream
-    public static InputStream Drawable2InputStream(Drawable d) {
-        Bitmap bitmap = drawable2Bitmap(d);
-        return Bitmap2InputStream(bitmap);
-    }
-
-    // InputStream转换成Drawable
-    public static Drawable InputStream2Drawable(Context context, InputStream is) {
-        Bitmap bitmap = InputStream2Bitmap(is);
-        return bitmap2Drawable(context, bitmap);
-    }
-
-    // Drawable转换成byte[]
-    public static byte[] Drawable2Bytes(Drawable d) {
-        Bitmap bitmap = drawable2Bitmap(d);
-        return Bitmap2Bytes(bitmap);
-    }
-
-    // byte[]转换成Drawable
-    public static Drawable Bytes2Drawable(Context context, byte[] b) {
-        Bitmap bitmap = Bytes2Bitmap(b);
-        return bitmap2Drawable(context, bitmap);
-    }
-
-    // Bitmap转换成byte[]
-    public static byte[] Bitmap2Bytes(Bitmap bm) {
-        ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        bm.compress(Bitmap.CompressFormat.PNG, 100, baos);
-        return baos.toByteArray();
-    }
-
-    // byte[]转换成Bitmap
-    public static Bitmap Bytes2Bitmap(byte[] b) {
-        if (b.length != 0) {
-            return BitmapFactory.decodeByteArray(b, 0, b.length);
-        }
-        return null;
-    }
-
-    // Drawable转换成Bitmap
-    public static Bitmap drawable2Bitmap(Drawable drawable) {
-        Bitmap bitmap = Bitmap.createBitmap(
-                drawable.getIntrinsicWidth(),
-                drawable.getIntrinsicHeight(),
-                drawable.getOpacity() != PixelFormat.OPAQUE
-                        ? Bitmap.Config.ARGB_8888 : Bitmap.Config.RGB_565);
-        Canvas canvas = new Canvas(bitmap);
-        drawable.setBounds(0, 0, drawable.getIntrinsicWidth(),
-                drawable.getIntrinsicHeight());
-        drawable.draw(canvas);
-        return bitmap;
-    }
-
-    // Bitmap转换成Drawable
-    public static Drawable bitmap2Drawable(Context context, Bitmap bitmap) {
-        return new BitmapDrawable(context.getResources(), bitmap);
-    }
-
-    public static Bitmap resource2Bitmap(Context context, int drawId) {
-        return BitmapFactory.decodeResource(context.getResources(), drawId);
-    }
-
-    public static Bitmap clipBitmap(Bitmap bitmap) {
-        if (bitmap == null) {
-            return null;
-        }
-
-        int w = bitmap.getWidth();
-        int h = bitmap.getHeight();
-
-        Bitmap bmp = Bitmap.createBitmap(bitmap, 0, 0, w / 3, h);
-        if (bmp != null && !bmp.equals(bitmap) && !bitmap.isRecycled()) {
-            bitmap.recycle();
-        }
-        return bmp;
-    }
-
-    public static int calculateInSampleSize(
-            BitmapFactory.Options options, int reqWidth, int reqHeight) {
-        // Raw height and width of image
-        final int height = options.outHeight;
-        final int width = options.outWidth;
-        int inSampleSize = 1;
-
-        if (height > reqHeight || width > reqWidth) {
-
-            final int halfHeight = height / 2;
-            final int halfWidth = width / 2;
-
-            // Calculate the largest inSampleSize value that is a power of 2 and keeps both
-            // height and width larger than the requested height and width.
-            while ((halfHeight / inSampleSize) > reqHeight
-                    && (halfWidth / inSampleSize) > reqWidth) {
-                inSampleSize *= 2;
-            }
-        }
-        return inSampleSize;
-    }
-
-    // 获取Bitmap
-    public static Bitmap decodeSampledBitmapFromResource(Resources res, int resId,
-                                                         int reqWidth, int reqHeight) {
-        // First decode with inJustDecodeBounds=true to check dimensions
-        final BitmapFactory.Options options = new BitmapFactory.Options();
-        options.inJustDecodeBounds = true;
-        BitmapFactory.decodeResource(res, resId, options);
-
-        // Calculate inSampleSize
-        options.inSampleSize = calculateInSampleSize(options, reqWidth, reqHeight);
-
-        // Decode bitmap with inSampleSize set
-        options.inJustDecodeBounds = false;
-        return BitmapFactory.decodeResource(res, resId, options);
-    }
-
-    // resource id to drawable
-    public static Drawable resourceId2Drawable(Context context, int resId) {
-        return ContextCompat.getDrawable(context, resId);
     }
 }
