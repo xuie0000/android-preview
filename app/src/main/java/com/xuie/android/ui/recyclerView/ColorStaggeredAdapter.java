52da46310a48c066f78e08ec8bc7076752ba44ac
xujie <xujie@yf-space.com>
2017-06-01 14:51:54 +0800

    cursor adapter

--- app/src/main/java/com/xuie/android/ui/recyclerView/ColorStaggeredAdapter.java
+++ app/src/main/java/com/xuie/android/ui/recyclerView/ColorStaggeredAdapter.java
@@ -9,7 +9,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-import com.orhanobut.logger.Logger;
 import com.xuie.android.R;
 
 import butterknife.BindView;
