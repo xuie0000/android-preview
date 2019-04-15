8628cbb5d62c5994dc32c10bfca8303332fcca19
Jie Xu <xujie@anzer.com.cn>
2019-04-15 13:30:57 +0800

    paging有点问题

--- app/src/main/java/com/xuie/android/ui/recycler/paging/CheeseDb.kt
+++ app/src/main/java/com/xuie/android/ui/recycler/paging/CheeseDb.kt
@@ -190,4 +190,4 @@ private val CHEESE_DATA = arrayListOf(
     "Washed Rind Cheese (Australian)", "Waterloo", "Weichkaese", "Wellington",
     "Wensleydale", "White Stilton", "Whitestone Farmhouse", "Wigmore", "Woodside Cabecou",
     "Xanadu", "Xynotyro", "Yarg Cornish", "Yarra Valley Pyramid", "Yorkshire Blue",
-    "Zamorano", "Zanetti Grana Padano", "Zanetti Parmigiano Reggiano");
+    "Zamorano", "Zanetti Grana Padano", "Zanetti Parmigiano Reggiano")
