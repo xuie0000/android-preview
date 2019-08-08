ede4214b5b1f327d9517bb69099900a4155b8f80
Jie Xu <xujie@anzer.com.cn>
2019-08-08 15:13:45 +0800

    update

--- library/src/main/java/xuk/util/ByteUtils.java
+++ library/src/main/java/xuk/util/ByteUtils.java
@@ -1,61 +1,20 @@
 package xuk.util;
 
 /**
+ * char在java中是2个字节。java采用unicode，2个字节（16位）来表示一个字符。
+ *
  * @author Jie Xu
- * @date 16-1-14
+ * @date 2019/8/8
  */
 public class ByteUtils {
-    /**
-     * Convert byte[] to hex string. 把字节数组转化为字符串
-     * 这里我们可以将byte转换成int，然后利用Integer.toHexString(int)来转换成16进制字符串。
-     *
-     * @param src byte[] data
-     * @return hex string
-     */
-    public static String bytesToHexString(byte[] src) {
-        StringBuilder stringBuilder = new StringBuilder("");
-        if (src == null || src.length <= 0) {
-            return null;
-        }
-        for (int i = 0; i < src.length; i++) {
-            int v = src[i] & 0xFF;
-            String hv = Integer.toHexString(v);
-            if (hv.length() < 2) {
-                stringBuilder.append(0);
-            }
-            stringBuilder.append(hv + " ");
-        }
-        return stringBuilder.toString();
+    public static byte[] charToByte(char c) {
+        byte[] b = new byte[2];
+        b[0] = (byte) ((c & 0xFF00) >> 8);
+        b[1] = (byte) (c & 0xFF);
+        return b;
     }
 
-    /**
-     * Convert hex string to byte[]   把为字符串转化为字节数组
-     *
-     * @param hexString the hex string
-     * @return byte[]
-     */
-    public static byte[] hexStringToBytes(String hexString) {
-        if (hexString == null || hexString.equals("")) {
-            return null;
-        }
-        hexString = hexString.toUpperCase();
-        int length = hexString.length() / 2;
-        char[] hexChars = hexString.toCharArray();
-        byte[] d = new byte[length];
-        for (int i = 0; i < length; i++) {
-            int pos = i * 2;
-            d[i] = (byte) (charToByte(hexChars[pos]) << 4 | charToByte(hexChars[pos + 1]));
-        }
-        return d;
-    }
-
-    /**
-     * Convert char to byte
-     *
-     * @param c char
-     * @return byte
-     */
-    private static byte charToByte(char c) {
-        return (byte) "0123456789ABCDEF".indexOf(c);
+    public static char byteToChar(byte[] b) {
+        return (char) (((b[0] & 0xFF) << 8) | (b[1] & 0xFF));
     }
 }
