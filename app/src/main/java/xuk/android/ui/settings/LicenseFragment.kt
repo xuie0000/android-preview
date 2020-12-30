05deca726653f243f774180c8db9bf61de56a5ca
Jie Xu <xujie@anzer.com.cn>
2020-12-30 14:54:14 +0800

    add settings fragment

---
+++ app/src/main/java/xuk/android/ui/settings/LicenseFragment.kt
@@ -0,0 +1,8 @@
+package xuk.android.ui.settings
+
+import androidx.fragment.app.Fragment
+import xuk.android.R
+
+class LicenseFragment : Fragment(R.layout.fragment_license) {
+
+}
--- app/src/main/java/xuk/android/ui/settings/SettingsFragment.kt
+++ app/src/main/java/xuk/android/ui/settings/SettingsFragment.kt
@@ -1,12 +1,18 @@
 package xuk.android.ui.settings
 
 import android.os.Bundle
+import androidx.navigation.fragment.findNavController
+import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
 import xuk.android.R
 
 class SettingsFragment : PreferenceFragmentCompat() {
 
   override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
-    setPreferencesFromResource(R.xml.root_preferences, rootKey)
+    setPreferencesFromResource(R.xml.settings_preferences, rootKey)
+    findPreference<Preference>("license")?.setOnPreferenceClickListener {
+      findNavController().navigate(R.id.nav_licence)
+      true
+    }
   }
 }
