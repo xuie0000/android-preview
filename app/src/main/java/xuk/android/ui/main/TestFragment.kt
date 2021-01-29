421ebdfd8e18a02ac7729b7a152e44a4bda3171f
Jie Xu <xujie@anzer.com.cn>
2021-01-29 16:53:31 +0800

    add a dialog test

--- app/src/main/java/xuk/android/ui/main/TestFragment.kt
+++ app/src/main/java/xuk/android/ui/main/TestFragment.kt
@@ -2,11 +2,17 @@ package xuk.android.ui.main
 
 import android.graphics.BitmapFactory
 import android.os.Bundle
+import android.text.InputFilter
+import android.text.InputType
 import android.view.View
+import android.view.WindowManager
 import android.view.animation.AnimationUtils
+import android.widget.EditText
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import kotlinx.android.synthetic.main.fragment_test.*
+import timber.log.Timber
 import xuk.android.R
 import xuk.android.widget.TopicThumbnailDrawable
 
@@ -16,9 +22,14 @@ class TestFragment : Fragment(R.layout.fragment_test) {
     super.onViewCreated(view, savedInstanceState)
 
     shakeSample()
-
     selectDrawableSample()
+    showDialogSample()
+  }
 
+  private fun showDialogSample() {
+    btn_dialog.setOnClickListener {
+      materialDialogTest()
+    }
   }
 
   private fun shakeSample() {
@@ -45,6 +56,34 @@ class TestFragment : Fragment(R.layout.fragment_test) {
     iv_foreground.setOnClickListener {
       it.isActivated = !it.isActivated
     }
+  }
+
+  private fun materialDialogTest() {
+    val editText = EditText(requireContext())
+    editText.inputType = InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_PASSWORD
+    editText.filters = arrayOf<InputFilter>(InputFilter.LengthFilter(11))
+    editText.hint = "hint"
+    editText.setPadding(16, 16, 16, 16)
+
+    val dialog = MaterialAlertDialogBuilder(requireContext())
+        .setTitle("跳过？")
+        .setView(editText)
+        .setMessage("下次是否不再显示登录界面？")
+        .setPositiveButton("YES") { _, _ ->
+          Timber.d("YES")
+        }
+        .setNegativeButton("CANCEL") { _, _ ->
+          Timber.d("Cancel")
+        }
+        .create()
+    dialog.window!!.attributes.width = 400
+
+    dialog.show()
+  }
+
+  private fun hideSoftInput() {
+    // 隐藏对话框
+    requireActivity().window.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN)
   }
 
 
