b753d5328de9c9f9fbefc57f1121f2590cf5894d
Jie Xu <xuie0000@163.com>
2021-04-06 16:01:30 +0800

    update

--- app/src/main/java/xuk/android/ui/recycler/paging/CheeseViewModel.kt
+++ app/src/main/java/xuk/android/ui/recycler/paging/CheeseViewModel.kt
@@ -2,8 +2,11 @@ package xuk.android.ui.recycler.paging
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.viewModelScope
 import androidx.paging.Pager
 import androidx.paging.PagingConfig
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class CheeseViewModel(app: Application) : AndroidViewModel(app) {
   private val dao = CheeseDb.get(app).cheeseDao()
@@ -17,8 +20,10 @@ class CheeseViewModel(app: Application) : AndroidViewModel(app) {
     pagingSourceFactory = { dao.allCheesesByName() }
   ).flow
 
-
-  fun insert(text: CharSequence) = ioThread {
+  /**
+   * TODO 为什么插入一条数据之后Item数量对不上了？
+   */
+  fun insert(text: CharSequence) = viewModelScope.launch(Dispatchers.IO) {
     dao.insert(Cheese(id = 0, name = text.toString()))
   }
 
