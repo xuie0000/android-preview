1e1eaa71bb15625cb922665551a7e682e71d041f
Jie Xu <xuie0000@163.com>
2021-07-27 09:32:24 +0800

    update gradle

--- app/src/main/java/xuk/android/App.kt
+++ app/src/main/java/xuk/android/App.kt
@@ -8,19 +8,12 @@ class App : Application() {
 
   override fun onCreate() {
     super.onCreate()
-    Timber.d("onCreate")
-
-    context = this
     Timber.plant(Timber.DebugTree())
-
     CrashReport.initCrashReport(applicationContext, BUG_LY_ID, false)
 
   }
 
   companion object {
-    lateinit var context: App
-      private set
-
     private const val BUG_LY_ID = "bc342fcfab"
   }
 }
