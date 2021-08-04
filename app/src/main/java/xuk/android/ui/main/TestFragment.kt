151a21621db79670118c87a2edf5f699bc584a3c
Jie Xu <xuie0000@163.com>
2021-08-04 11:02:07 +0800

    delete the boot sheet and dialog demo

--- app/src/main/java/xuk/android/ui/main/TestFragment.kt
+++ app/src/main/java/xuk/android/ui/main/TestFragment.kt
@@ -2,17 +2,11 @@ package xuk.android.ui.main
 
 import android.graphics.BitmapFactory
 import android.os.Bundle
-import android.text.InputFilter
-import android.text.InputType
 import android.view.View
-import android.view.WindowManager
 import android.view.animation.AnimationUtils
-import android.widget.EditText
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import by.kirich1409.viewbindingdelegate.viewBinding
-import com.google.android.material.dialog.MaterialAlertDialogBuilder
-import timber.log.Timber
 import xuk.android.R
 import xuk.android.databinding.FragmentTestBinding
 import xuk.android.widget.TopicThumbnailDrawable
@@ -26,13 +20,6 @@ class TestFragment : Fragment(R.layout.fragment_test) {
 
     shakeSample()
     selectDrawableSample()
-    showDialogSample()
-  }
-
-  private fun showDialogSample() {
-    binding.btnDialog.setOnClickListener {
-      materialDialogTest()
-    }
   }
 
   private fun shakeSample() {
@@ -59,34 +46,6 @@ class TestFragment : Fragment(R.layout.fragment_test) {
     binding.ivForeground.setOnClickListener {
       it.isActivated = !it.isActivated
     }
-  }
-
-  private fun materialDialogTest() {
-    val editText = EditText(requireContext())
-    editText.inputType = InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_PASSWORD
-    editText.filters = arrayOf<InputFilter>(InputFilter.LengthFilter(11))
-    editText.hint = "hint"
-    editText.setPadding(16, 16, 16, 16)
-
-    val dialog = MaterialAlertDialogBuilder(requireContext())
-      .setTitle("跳过？")
-      .setView(editText)
-      .setMessage("下次是否不再显示登录界面？")
-      .setPositiveButton("YES") { _, _ ->
-        Timber.d("YES")
-      }
-      .setNegativeButton("CANCEL") { _, _ ->
-        Timber.d("Cancel")
-      }
-      .create()
-    dialog.window!!.attributes.width = 400
-
-    dialog.show()
-  }
-
-  private fun hideSoftInput() {
-    // 隐藏对话框
-    requireActivity().window.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN)
   }
 
 
--- app/src/main/java/xuk/android/ui/transitions/SceneTransitionsActivity.kt
+++ app/src/main/java/xuk/android/ui/transitions/SceneTransitionsActivity.kt
@@ -5,18 +5,12 @@ import android.transition.Explode
 import android.transition.Fade
 import android.transition.Slide
 import android.view.MenuItem
-import android.view.View
 import android.view.animation.OvershootInterpolator
-import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
-import com.google.android.material.bottomsheet.BottomSheetBehavior
-import timber.log.Timber
 import xuk.android.databinding.ActivitySceneTransitionsBinding
-import xuk.android.util.dp2px
 
 class SceneTransitionsActivity : AppCompatActivity() {
 
-  private lateinit var bottomSheetBehavior: BottomSheetBehavior<LinearLayout>
   private lateinit var binding: ActivitySceneTransitionsBinding
 
   override fun onCreate(savedInstanceState: Bundle?) {
@@ -43,54 +37,6 @@ class SceneTransitionsActivity : AppCompatActivity() {
     setSupportActionBar(binding.toolbar)
     supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
-    initBottomSheetBehavior()
-
-    binding.fabButton.setOnClickListener {
-      bottomSheetBehavior.isHideable = true
-      when (bottomSheetBehavior.state) {
-        BottomSheetBehavior.STATE_EXPANDED -> bottomSheetBehavior.setState(BottomSheetBehavior.STATE_COLLAPSED)
-        BottomSheetBehavior.STATE_COLLAPSED -> bottomSheetBehavior.setState(BottomSheetBehavior.STATE_HIDDEN)
-        BottomSheetBehavior.STATE_HIDDEN -> bottomSheetBehavior.state =
-          BottomSheetBehavior.STATE_COLLAPSED
-        BottomSheetBehavior.STATE_DRAGGING -> {
-        }
-        BottomSheetBehavior.STATE_HALF_EXPANDED -> {
-        }
-        BottomSheetBehavior.STATE_SETTLING -> {
-        }
-      }
-      bottomSheetBehavior.setHideable(false)
-    }
-  }
-
-  private fun initBottomSheetBehavior() {
-    // init the bottom sheet behavior
-    bottomSheetBehavior = BottomSheetBehavior.from(binding.bottomSheet)
-
-    // change the state of the bottom sheet
-    /**
-     * @link BottomSheetBehavior.STATE_COLLAPSED 收紧
-     * @link BottomSheetBehavior.STATE_EXPANDED 展开
-     * @link BottomSheetBehavior.STATE_HIDDEN 隐藏
-     */
-    bottomSheetBehavior.state = BottomSheetBehavior.STATE_COLLAPSED
-
-    // set the peek height
-    bottomSheetBehavior.peekHeight = dp2px(80f)
-
-    // set hide or not
-    bottomSheetBehavior.isHideable = false
-
-    // set callback for changes
-    bottomSheetBehavior.addBottomSheetCallback(object : BottomSheetBehavior.BottomSheetCallback() {
-      override fun onStateChanged(bottomSheet: View, newState: Int) {
-        Timber.d("onStateChanged: $newState")
-      }
-
-      override fun onSlide(bottomSheet: View, slideOffset: Float) {
-
-      }
-    })
   }
 
   override fun onOptionsItemSelected(item: MenuItem): Boolean {
